d623379eb0077f47cad4b7e62052f93e32653349
hunor64 <nhunor06@gmail.com>
2024-09-25 13:13:37 +0200

    Butt works

--- desserts-frontend/src/components/AdddToCartBtn.tsx
+++ desserts-frontend/src/components/AdddToCartBtn.tsx
@@ -1,9 +1,27 @@
-const AdddToCartBtn = () => {
-  return (
-    <div>
-        <button>Add to Cart</button>
-    </div>
-  )
-}
+import { useState } from "react";
 
-export default AdddToCartBtn
+const AdddToCartBtn = () => {
+  const [quantity, setQuantity] = useState(0);
+
+  const increase = () => {
+    setQuantity(quantity + 1);
+  };
+  const decrease = () => {
+    setQuantity(quantity - 1);
+  };
+
+  return (
+    <> {quantity === 0 ? 
+        <div onClick={increase}>Add to Cart</div> :
+        <div>        
+        <button onClick={decrease}>-</button>
+        {quantity}
+        <button onClick={increase}>+</button>
+      </div>
+    }
+
+    </>
+);
+};
+
+export default AdddToCartBtn;
