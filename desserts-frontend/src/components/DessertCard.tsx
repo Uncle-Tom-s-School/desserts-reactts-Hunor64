d7d4a0192da0d9690e1cc1a8220e19c386d2af1b
hunor64 <nhunor06@gmail.com>
2024-10-02 13:02:38 +0200

    Added cart.tsx

--- desserts-frontend/src/components/DessertCard.tsx
+++ desserts-frontend/src/components/DessertCard.tsx
@@ -19,7 +19,7 @@ const DessertCard = (props: DessertCardProps) => {
         <AdddToCartBtn />
         <h4>{props.category}</h4>
         <h2>{props.name}</h2>
-        <h3>{props.price}</h3>
+        <h3>${props.price}</h3>
     </div>
   )
 }
