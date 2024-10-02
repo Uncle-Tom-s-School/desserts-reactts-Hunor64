851c6cbb285c3e034a301bccb6cb30d205aca959
hunor64 <nhunor06@gmail.com>
2024-10-02 13:06:31 +0200

    Cart update

--- desserts-frontend/src/components/Cart.tsx
+++ desserts-frontend/src/components/Cart.tsx
@@ -1,10 +1,14 @@
-const Cart = () => {
-  return (
-    
-    <div>
+import { useState } from 'react';
 
+const Cart = () => {
+    const [cart,setCart] = useState([]);
+
+  return (
+    <div>
+        <h2>Your Cart ({cart.length})</h2>
     </div>
   )
 }
+
 
 export default Cart
